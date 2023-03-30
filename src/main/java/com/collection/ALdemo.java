c64a6b6bcba98f54a34ac047e31071f9c274a1a2
Kalyaniwd <kalyanidipakwalunj@gmail.com>
2023-03-30 11:53:14 +0530

    This is my first commit

---
+++ src/main/java/com/collection/ALdemo.java
@@ -0,0 +1,32 @@
+package com.collection;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ALdemo {
+	public static void main(String[] args) {
+Collection c;
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add("Hello");
+		al.add(10.21f);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(true);
+		al.add(null);
+
+		
+
+		System.out.println(al.iterator());
+		
+	}
+
+}
